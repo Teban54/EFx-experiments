6345f1ef58afb6179664468651fe7ef46b5ce66e
Xingyu Chen <xc77@duke.edu>
2019-03-07 22:39:11 -0500

    Changing Notation

--- src/main.py
+++ src/main.py
@@ -2,13 +2,14 @@ from Allocation import Allocation
 import numpy as np
 from expt import generate_utilities
 
-def brute_force(m, n, eval_func, log = True):
-    d = np.zeros((n+1, ))
-    leximin = Allocation(m, n, eval_matrix)
+
+def brute_force(n, m, eval_func, log = True):
+    d = np.zeros((m+1, ))
+    leximin = Allocation(n, m, eval_matrix)
     flag = False
     while d[0] == 0:
-        new_allocation = Allocation(m, n, eval_func)
-        for i in range(1, n+1):
+        new_allocation = Allocation(n, m, eval_func)
+        for i in range(1, m+1):
             new_allocation.allocate(d[i], i - 1)
         efx, pair = new_allocation.is_EFX()
         if efx and not flag:
@@ -19,9 +20,9 @@ def brute_force(m, n, eval_func, log = True):
 
         if leximin < new_allocation:
             leximin = new_allocation
-        j = n
+        j = m
         d[j] += 1
-        while d[j] == m:
+        while d[j] == n:
             d[j] = 0
             j -= 1
             d[j] += 1
@@ -37,8 +38,8 @@ def brute_force(m, n, eval_func, log = True):
         if log:
             print("Leximin Allocation is a EFX Allocation")
     else:
-        if log:
-            print("Leximin Allocation is not a EFX Allocation")
+        print(eval_func)
+        print("Leximin Allocation is not a EFX Allocation")
 
     return flag
 
@@ -47,7 +48,12 @@ if __name__ == "__main__":
     eval_matrix = np.array([[1, 2, 1], [1, 1, 2], [2, 1, 1]])
     # print(generate_utilities(2, 3, 4))
     flag = True
+    N = 5
+    M = 8
+    cnt = 0
     while flag:
-        eval_matrix = np.random.randint(5, size=(3, 3))
-        flag = brute_force(3, 3, eval_matrix, log=False)
+        cnt += 1
+        print(cnt)
+        eval_matrix = np.random.randint(5, size=(N, M))
+        flag = brute_force(N, M, eval_matrix, log=False)
     print(eval_matrix)
