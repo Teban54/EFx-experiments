cbbfccc2d32677f88f144bf234034165e678f472
Charles Lyu <liang.lyu@duke.edu>
2019-03-07 20:20:54 -0500

    Update rules for utility generation

--- src/expt.py
+++ src/expt.py
@@ -9,16 +9,17 @@ def generate_utilities(N, M):
     """
     Generate random utility functions for each player and each subset of goods.
 
-    Utilities are totally random except they obey the following two rules:
-    1) Ui(S) > 0 for all S that is a subset of all goods.
-    2) If S1 is a subset of S2, then Ui(S1) <= Ui(S2).
+    Utilities are totally random except they obey the following three rules:
+    1) Ui(emptyset) = 0.
+    2) Ui(S) > 0 for all S that is an nonempty subset of all goods.
+    3) If S1 is a subset of S2, then Ui(S1) <= Ui(S2).
 
     :param: N: Number of players
     :param: M: Number of goods
     :return: List of N dicts, each mapping all possible subset of goods to
         their utility values
     """
-
+    
 
 
 if __name__ == '__main__':
